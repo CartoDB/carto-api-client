125a822d1dc338161018105637669568f6c4e98d
Don McCurdy <donmccurdy@cartodb.com>
2024-10-02 12:27:32 -0400

    chore(release): v0.2.1-alpha.12 (#19)

--- scripts/postversion-commit.js
+++ scripts/postversion-commit.js
@@ -1,27 +1,27 @@
 import {execSync} from 'node:child_process';
-import assert from 'node:assert/strict';
+import {readFile} from 'node:fs/promises';
+import {resolve} from 'node:path';
+import {valid} from 'semver';
 
 /**
  * Utility for committing and tagging a release commit in
  * git, called as part of the `yarn postversion` script.
  */
 
-const version = process.env.VERSION;
+// Read and validate pkg.version.
+const pkgJSON = await readFile(resolve('./package.json'), 'utf-8');
+const version = 'v' + JSON.parse(pkgJSON).version;
+if (!valid(version)) {
+  throw new Error(`Invalid version, "${version}"`);
+}
 
-assert.match(
-  version,
-  /^v\d+\.\d+\.\d+[a-zA-Z0-9\._-]*$/,
-  'Missing or invalid process.env.VERSION'
-);
-
-const currentBranch = execSync('git rev-parse --abbrev-ref HEAD')
-  .toString()
-  .trim();
-
-if (currentBranch === 'main') {
+// Check out a branch if cutting a version from 'main'.
+const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+if (branch === 'main') {
   execSync(`git checkout -b 'release/${version}'`);
 }
 
+// Commit and tag.
 execSync('git add -u');
 execSync(`git commit -m 'chore(release): ${version}'`);
 execSync(`git tag -a ${version} -m ${version}`);
