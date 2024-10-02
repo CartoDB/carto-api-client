6a1b32f13dca40affc76a0caf99185e9ec339ac1
Don McCurdy <donmccurdy@cartodb.com>
2024-10-02 11:26:24 -0400

    fixes for postversion-commit.js

--- scripts/postversion-commit.js
+++ scripts/postversion-commit.js
@@ -1,18 +1,10 @@
 import {execSync} from 'node:child_process';
-import assert from 'node:assert/strict';
+import {version} from '../package.json';
 
 /**
  * Utility for committing and tagging a release commit in
  * git, called as part of the `yarn postversion` script.
  */
-
-const version = process.env.VERSION;
-
-assert.match(
-  version,
-  /^v\d+\.\d+\.\d+[a-zA-Z0-9\._-]*$/,
-  'Missing or invalid process.env.VERSION'
-);
 
 const currentBranch = execSync('git rev-parse --abbrev-ref HEAD')
   .toString()
