181da4d4407efb90b4a8f15b468c34a9e18661a5
Don McCurdy <donmccurdy@cartodb.com>
2024-10-02 11:41:16 -0400

    chore(release): undefined

--- scripts/postversion-commit.js
+++ scripts/postversion-commit.js
@@ -6,7 +6,7 @@ import {execSync} from 'node:child_process';
  */
 
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-const {version} = await import('../package.json');
+const {version} = await import('../package.json', {assert: {type: 'json'}});
 
 if (branch === 'main') {
   execSync(`git checkout -b 'release/${version}'`);
