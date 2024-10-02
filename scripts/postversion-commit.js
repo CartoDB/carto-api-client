22704b2f839476beee6a9f0d2dddcd28b5e42c8d
Don McCurdy <donmccurdy@cartodb.com>
2024-10-02 11:50:24 -0400

    chore(release): 0.2.1-alpha.5

--- scripts/postversion-commit.js
+++ scripts/postversion-commit.js
@@ -1,4 +1,6 @@
 import {execSync} from 'node:child_process';
+import {readFile} from 'node:fs/promises';
+import {resolve} from 'node:path';
 import {valid} from 'semver';
 
 /**
@@ -6,16 +8,20 @@ import {valid} from 'semver';
  * git, called as part of the `yarn postversion` script.
  */
 
-const {version} = await import('../package.json', {assert: {type: 'json'}});
+// Read and validate pkg.version.
+const pkgJSON = await readFile(resolve('./package.json'), 'utf-8');
+const {version} = JSON.parse(pkgJSON);
 if (!valid(version)) {
   throw new Error(`Invalid version, "${version}"`);
 }
 
+// Check out a branch if cutting a version from 'main'.
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 if (branch === 'main') {
   execSync(`git checkout -b 'release/${version}'`);
 }
 
+// Commit and tag.
 execSync('git add -u');
 execSync(`git commit -m 'chore(release): ${version}'`);
 execSync(`git tag -a ${version} -m ${version}`);
