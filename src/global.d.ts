22a00f24ce714919b82c2520f8df4c2b929c8dc3
Don McCurdy <donmccurdy@cartodb.com>
2024-10-07 16:08:40 -0400

    feat(deps): Add @deck.gl/carto as prod dependency

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,7 @@
 /**
- * Injected by microbundle. The `@deck.gl/core` checkVersion()
- * function will detect globally-defined 'VERSION' variables, and is inlined
- * in the client bundle, so the 'VERSION' name should be avoided.
+ * Injected by microbundle, with:
+ * ```
+ * --define __CARTO_API_CLIENT_VERSION=$npm_package_version
+ * ```
  */
 declare const __CARTO_API_CLIENT_VERSION: string;
