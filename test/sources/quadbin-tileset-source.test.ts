b6ac088684171b011d0a409d166759cee458cc28
Don McCurdy <donmccurdy@cartodb.com>
2024-10-29 17:09:10 -0400

    migrate sources tests

---
+++ test/sources/quadbin-tileset-source.test.ts
@@ -0,0 +1,63 @@
+// deck.gl
+// SPDX-License-Identifier: MIT
+// Copyright (c) vis.gl contributors
+
+import {quadbinTilesetSource} from '@carto/api-client';
+import {describe, afterEach, vi, test, expect, beforeEach} from 'vitest';
+
+const CACHE = 'quadbin-tileset-source-test';
+
+const INIT_RESPONSE = {
+  tilejson: {url: [`https://xyz.com?format=tilejson&cache=${CACHE}`]},
+};
+
+const TILESET_RESPONSE = {
+  tilejson: '2.2.0',
+  tiles: ['https://xyz.com/{z}/{x}/{y}?formatTiles=binary'],
+  tilestats: {layers: []},
+};
+
+describe('quadbinTilesetSource', () => {
+  beforeEach(() => {
+    const mockFetch = vi
+      .fn()
+      .mockReturnValueOnce(
+        Promise.resolve({ok: true, json: () => Promise.resolve(INIT_RESPONSE)})
+      )
+      .mockReturnValueOnce(
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(TILESET_RESPONSE),
+        })
+      );
+
+    vi.stubGlobal('fetch', mockFetch);
+  });
+
+  afterEach(() => void vi.restoreAllMocks());
+
+  test('default', async () => {
+    const tilejson = await quadbinTilesetSource({
+      connectionName: 'carto_dw',
+      accessToken: '<token>',
+      tableName: 'a.b.quadbin_tileset',
+    });
+
+    expect(vi.mocked(fetch)).toHaveBeenCalledTimes(2);
+
+    const [[initURL], [tilesetURL]] = vi.mocked(fetch).mock.calls;
+
+    expect(initURL).toMatch(/v3\/maps\/carto_dw\/tileset/);
+    expect(initURL).toMatch(/name=a.b.quadbin_tileset/);
+
+    expect(tilesetURL).toMatch(
+      /^https:\/\/xyz\.com\/\?format\=tilejson\&cache\=/
+    );
+
+    expect(tilejson).toBeTruthy();
+    expect(tilejson.tiles).toEqual([
+      'https://xyz.com/{z}/{x}/{y}?formatTiles=binary',
+    ]);
+    expect(tilejson.accessToken).toBe('<token>');
+  });
+});
---
+++ test/sources/vector-table-source.test.ts
@@ -0,0 +1,78 @@
+// deck.gl
+// SPDX-License-Identifier: MIT
+// Copyright (c) vis.gl contributors
+
+import {WidgetTableSource, vectorTableSource} from '@carto/api-client';
+import {describe, afterEach, vi, test, expect, beforeEach} from 'vitest';
+
+const CACHE = 'vector-table-source-test';
+
+const INIT_RESPONSE = {
+  tilejson: {url: [`https://xyz.com?format=tilejson&cache=${CACHE}`]},
+};
+
+const TILESET_RESPONSE = {
+  tilejson: '2.2.0',
+  tiles: ['https://xyz.com/{z}/{x}/{y}?formatTiles=binary'],
+  tilestats: {layers: []},
+};
+
+describe('vectorTableSource', () => {
+  beforeEach(() => {
+    const mockFetch = vi
+      .fn()
+      .mockReturnValueOnce(
+        Promise.resolve({ok: true, json: () => Promise.resolve(INIT_RESPONSE)})
+      )
+      .mockReturnValueOnce(
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(TILESET_RESPONSE),
+        })
+      );
+
+    vi.stubGlobal('fetch', mockFetch);
+  });
+
+  afterEach(() => void vi.restoreAllMocks());
+
+  test('default', async () => {
+    const tilejson = await vectorTableSource({
+      connectionName: 'carto_dw',
+      accessToken: '<token>',
+      tableName: 'a.b.vector_table',
+      columns: ['a', 'b'],
+      spatialDataColumn: 'mygeom',
+    });
+
+    expect(vi.mocked(fetch)).toHaveBeenCalledTimes(2);
+
+    const [[initURL], [tilesetURL]] = vi.mocked(fetch).mock.calls;
+
+    expect(initURL).toMatch(/v3\/maps\/carto_dw\/table/);
+    expect(initURL).toMatch(/name=a\.b\.vector_table/);
+    expect(initURL).toMatch(/columns=a%2Cb/);
+    expect(initURL).toMatch(/spatialDataColumn=mygeom/);
+    expect(initURL).toMatch(/spatialDataType=geo/);
+
+    expect(tilesetURL).toMatch(
+      /^https:\/\/xyz\.com\/\?format\=tilejson\&cache\=/
+    );
+
+    expect(tilejson).toBeTruthy();
+    expect(tilejson.tiles).toEqual([
+      'https://xyz.com/{z}/{x}/{y}?formatTiles=binary',
+    ]);
+    expect(tilejson.accessToken).toBe('<token>');
+  });
+
+  test('widgetSource', async () => {
+    const {widgetSource} = await vectorTableSource({
+      accessToken: '<token>',
+      connectionName: 'carto_dw',
+      tableName: 'my-table',
+    });
+
+    expect(widgetSource).toBeInstanceOf(WidgetTableSource);
+  });
+});
