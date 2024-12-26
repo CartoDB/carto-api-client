515265e73236993a51f8bcc346ade46fadddf4bf
Don McCurdy <donmccurdy@cartodb.com>
2024-12-26 15:18:47 -0500

    tileset formula example

--- src/sources/vector-tileset-source.ts
+++ src/sources/vector-tileset-source.ts
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
+import {
+  WidgetTilesetSource,
+  WidgetTilesetSourceResult,
+} from '../widget-sources';
 import {baseSource} from './base-source';
 import type {
   SourceOptions,
@@ -12,7 +16,8 @@ import type {
 export type VectorTilesetSourceOptions = SourceOptions & TilesetSourceOptions;
 type UrlParameters = {name: string};
 
-export type VectorTilesetSourceResponse = TilejsonResult;
+export type VectorTilesetSourceResponse = TilejsonResult &
+  WidgetTilesetSourceResult;
 
 export const vectorTilesetSource = async function (
   options: VectorTilesetSourceOptions
@@ -20,9 +25,10 @@ export const vectorTilesetSource = async function (
   const {tableName} = options;
   const urlParameters: UrlParameters = {name: tableName};
 
-  return baseSource<UrlParameters>(
-    'tileset',
-    options,
-    urlParameters
+  return baseSource<UrlParameters>('tileset', options, urlParameters).then(
+    (result) => ({
+      ...(result as TilejsonResult),
+      widgetSource: new WidgetTilesetSource(options),
+    })
   ) as Promise<VectorTilesetSourceResponse>;
 };
