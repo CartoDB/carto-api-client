1e826ab0462e0feefc3dfe61eedd69e95488f513
Don McCurdy <donmccurdy@cartodb.com>
2025-01-15 16:50:08 -0500

    feat(WidgetRasterSource): initial commit

--- src/widget-sources/index.ts
+++ src/widget-sources/index.ts
@@ -1,4 +1,5 @@
 export * from './widget-query-source.js';
+export * from './widget-raster-source.js';
 export * from './widget-remote-source.js';
 export * from './widget-table-source.js';
 export * from './widget-tileset-source.js';
