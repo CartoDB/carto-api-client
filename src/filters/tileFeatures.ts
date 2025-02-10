98664a1d47eb4a45c4c5ce05dccf4248c15ec77e
Don McCurdy <donmccurdy@cartodb.com>
2025-02-10 16:21:53 -0500

    fixup! clean up tileFeatures types

--- src/filters/tileFeatures.ts
+++ src/filters/tileFeatures.ts
@@ -1,7 +1,7 @@
 import {SpatialFilter, SpatialIndexTile, Tile} from '../types';
 import {tileFeaturesGeometries} from './tileFeaturesGeometries';
 import {tileFeaturesSpatialIndex} from './tileFeaturesSpatialIndex';
-import {SpatialIndex, TileFormat} from '../constants';
+import {TileFormat} from '../constants';
 import {DEFAULT_GEO_COLUMN} from '../constants-internal';
 import {FeatureData} from '../types-internal';
 import {SpatialDataType} from '../sources/types';
@@ -12,7 +12,7 @@ export type TileFeatures = {
   tileFormat: TileFormat;
   spatialDataType: SpatialDataType;
   spatialDataColumn?: string;
-  spatialFilter?: SpatialFilter;
+  spatialFilter: SpatialFilter;
   uniqueIdProperty?: string;
   options?: TileFeatureExtractOptions;
 };
@@ -32,12 +32,6 @@ export function tileFeatures({
   spatialDataType,
   options = {},
 }: TileFeatures): FeatureData[] {
-  // TODO(cleanup): Is an empty response the expected result when spatialFilter
-  // is omitted? Why not make the parameter required, or return the full input?
-  if (!spatialFilter) {
-    return [];
-  }
-
   if (spatialDataType !== 'geo') {
     return tileFeaturesSpatialIndex({
       tiles: tiles as SpatialIndexTile[],
