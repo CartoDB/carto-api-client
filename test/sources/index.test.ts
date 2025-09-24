bc7d4105c7702b199faf511e72fde40c46dc4399
felixpalmer <felixpalmer@gmail.com>
2025-09-24 16:01:32 +0200

    fix: Export CARTO_SOURCES (#243)

---
+++ test/sources/index.test.ts
@@ -0,0 +1,35 @@
+import {describe, expect, test} from 'vitest';
+import {CARTO_SOURCES} from '@carto/api-client';
+
+describe('sources index', () => {
+  test('CARTO_SOURCES exports all source functions', () => {
+    expect(CARTO_SOURCES).toBeDefined();
+    expect(typeof CARTO_SOURCES).toBe('object');
+
+    const expectedSources = [
+      'boundaryQuerySource',
+      'boundaryTableSource',
+      'h3QuerySource',
+      'h3TableSource',
+      'h3TilesetSource',
+      'quadbinQuerySource',
+      'quadbinTableSource',
+      'quadbinTilesetSource',
+      'rasterSource',
+      'trajectoryQuerySource',
+      'trajectoryTableSource',
+      'vectorQuerySource',
+      'vectorTableSource',
+      'vectorTilesetSource',
+    ];
+
+    expectedSources.forEach((sourceName) => {
+      expect(CARTO_SOURCES).toHaveProperty(sourceName);
+      expect(
+        typeof CARTO_SOURCES[sourceName as keyof typeof CARTO_SOURCES]
+      ).toBe('function');
+    });
+
+    expect(Object.keys(CARTO_SOURCES)).toHaveLength(expectedSources.length);
+  });
+});
